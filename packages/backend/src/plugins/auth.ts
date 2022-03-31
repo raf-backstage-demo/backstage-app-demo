2d0be4936210b10644fb4d23f951a6f770b0b475
raffaelespazzoli <raffaele.spazzoli@gmail.com>
2022-03-30 21:19:16 -0400

    f
    Signed-off-by: raffaelespazzoli <raffaele.spazzoli@gmail.com>

--- packages/backend/src/plugins/auth.ts
+++ packages/backend/src/plugins/auth.ts
@@ -1,8 +1,8 @@
 import { createRouter } from '@backstage/plugin-auth-backend';
 import { Router } from 'express';
 import { PluginEnvironment } from '../types';
-import { createGithubProvider  } from '@backstage/plugin-auth-backend';
-import { DEFAULT_NAMESPACE, stringifyEntityRef } from '@backstage/catalog-model';
+// import { createGithubProvider  } from '@backstage/plugin-auth-backend';
+// import { DEFAULT_NAMESPACE, stringifyEntityRef } from '@backstage/catalog-model';
 
 export default async function createPlugin(
   env: PluginEnvironment,
@@ -13,33 +13,33 @@ export default async function createPlugin(
     database: env.database,
     discovery: env.discovery,
     tokenManager: env.tokenManager,
-    providerFactories: {
-      github: createGithubProvider({
-        signIn: {
-          resolver: async ({ profile: { email } }, ctx) => {
-            if (email===undefined) throw TypeError("undefined email")
-            const [id] = email.split('@');
-            const userEntityRef = stringifyEntityRef({
-              kind: 'User',
-              namespace: DEFAULT_NAMESPACE,
-              name: id,
-            });
-            // Fetch from an external system that returns entity claims like:
-            // ['user:default/breanna.davison', ...]
-            //const ent = await externalSystemClient.getUsernames(email);
+    // providerFactories: {
+    //   github: createGithubProvider({
+    //     signIn: {
+    //       resolver: async ({ profile: { email } }, ctx) => {
+    //         if (email===undefined) throw TypeError("undefined email")
+    //         const [id] = email.split('@');
+    //         const userEntityRef = stringifyEntityRef({
+    //           kind: 'User',
+    //           namespace: DEFAULT_NAMESPACE,
+    //           name: id,
+    //         });
+    //         // Fetch from an external system that returns entity claims like:
+    //         // ['user:default/breanna.davison', ...]
+    //         //const ent = await externalSystemClient.getUsernames(email);
 
-            // Resolve group membership from the Backstage catalog
-            const fullEnt = await ctx.catalogIdentityClient.resolveCatalogMembership({
-              entityRefs: [userEntityRef],
-              logger: ctx.logger,
-            });
-            const token = await ctx.tokenIssuer.issueToken({
-              claims: { sub: userEntityRef, ent: fullEnt },
-            });
-            return { id, token };
-          },
-        }
-      })
-    }    
+    //         // Resolve group membership from the Backstage catalog
+    //         const fullEnt = await ctx.catalogIdentityClient.resolveCatalogMembership({
+    //           entityRefs: [userEntityRef],
+    //           logger: ctx.logger,
+    //         });
+    //         const token = await ctx.tokenIssuer.issueToken({
+    //           claims: { sub: userEntityRef, ent: fullEnt },
+    //         });
+    //         return { id, token };
+    //       },
+    //     }
+    //   })
+    // }    
   });
 }
