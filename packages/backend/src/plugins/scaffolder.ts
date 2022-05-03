6849a712ccb64529e54a266234c969d50a9fb323
raffaelespazzoli <raffaele.spazzoli@gmail.com>
2022-05-03 14:06:59 -0400

    f
    Signed-off-by: raffaelespazzoli <raffaele.spazzoli@gmail.com>

--- packages/backend/src/plugins/scaffolder.ts
+++ packages/backend/src/plugins/scaffolder.ts
@@ -3,6 +3,11 @@ import { createRouter } from '@backstage/plugin-scaffolder-backend';
 import { Router } from 'express';
 import type { PluginEnvironment } from '../types';
 
+import { createBuiltinActions } from '@backstage/plugin-scaffolder-backend';
+import { ScmIntegrations } from '@backstage/integration';
+import { createHttpBackstageAction } from "@roadiehq/scaffolder-backend-module-http-request";
+import { createZipAction, createWriteFileAction, createAppendFileAction, createSleepAction } from '@roadiehq/scaffolder-backend-module-utils';
+
 export default async function createPlugin(
   env: PluginEnvironment,
 ): Promise<Router> {
@@ -10,11 +15,28 @@ export default async function createPlugin(
     discoveryApi: env.discovery,
   });
 
+  const integrations = ScmIntegrations.fromConfig(env.config);
+
+  const builtInActions = createBuiltinActions({
+    integrations,
+    catalogClient,
+    config: env.config,
+    reader: env.reader,
+  });
+  
+  const actions = [...builtInActions, 
+    createHttpBackstageAction({ config: env.config }), 
+    createWriteFileAction(),
+    createSleepAction(),
+    createZipAction(),
+    createAppendFileAction()];
+
   return await createRouter({
+    catalogClient,
+    actions,
     logger: env.logger,
     config: env.config,
     database: env.database,
     reader: env.reader,
-    catalogClient,
   });
 }
